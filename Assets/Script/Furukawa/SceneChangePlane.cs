8a052b742744f65818bc1c1d50a121a5a1881cdf
Ryouta32 <ryouta32ryouta@gmail.com>
2024-08-09 16:57:45 +0900

    いったんシーンだけ作りましたb

---
+++ Assets/Script/Furukawa/SceneChangePlane.cs
@@ -0,0 +1,19 @@
+using Meta.XR.Editor.Tags;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class SceneChangePlane : MonoBehaviour
+{
+    [SerializeField, Tag] private string _tagName;
+    [SerializeField] SceneName.sceneName name;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if(collision.gameObject.CompareTag(_tagName.ToString()))
+        SceneManager.LoadScene(name.ToString());
+    }
+}
---
+++ Assets/Script/Furukawa/SceneName.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneName
+{
+    public enum sceneName
+    {
+        MainScene, StartScene, EntranceScene
+    }
+}
