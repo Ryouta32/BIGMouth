417690b9026e618e1a92eb6fa56eb1be40b0c3c1
Ryouta32 <ryouta32ryouta@gmail.com>
2024-09-10 21:09:54 +0900

    仮で出してたログ消しときます

--- Assets/Script/Furukawa/bouScript.cs
+++ Assets/Script/Furukawa/bouScript.cs
@@ -24,7 +24,6 @@ public class bouScript : MonoBehaviour
 
         transform.LookAt(ovr.leftHandAnchor.position);
         oldPos= ovr.rightHandAnchor.position;
-        DebugText.LogText.Log(transform.position);
     }
 
     public Vector3 pos => ovr.leftHandAnchor.position - ovr.rightHandAnchor.position;
