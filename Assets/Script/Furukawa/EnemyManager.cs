d285d44200ee0a2baf9d669be941b22f48b43f2c
Ryouta32 <90999621+Ryouta32@users.noreply.github.com>
2024-09-13 16:35:08 +0900

    敵の行動調整

--- Assets/Script/Furukawa/EnemyManager.cs
+++ Assets/Script/Furukawa/EnemyManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class EnemyManager : MonoBehaviour
 {
    [SerializeField] List<GameObject> enemys;
-
+    [SerializeField] int spawnLimit;
     public void ClearCheck() {
         if (enemys.Count == 0)
         {
@@ -18,4 +18,5 @@ public class EnemyManager : MonoBehaviour
     public void AddEnemys(GameObject obj) => enemys.Add(obj);
     public void DestroyEnemys(GameObject obj) { enemys.Remove(obj); ClearCheck(); }
     public void ResetEnemys() => enemys=new List<GameObject>();
+    public bool SpawnCheck() => enemys.Count>=spawnLimit;
 }
