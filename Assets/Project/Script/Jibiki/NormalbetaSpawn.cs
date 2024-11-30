8711ed24f3bc73411685bbb2fd7dfbab359fc82f
Fumirin16 <90897959+Fumirin16@users.noreply.github.com>
2024-11-30 17:28:53 +0900

    音とエフェクト修正しました

---
+++ Assets/Project/Script/Jibiki/NormalbetaSpawn.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+/* 床の座標取得して */
+
+public class NormalbetaSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private OVRSceneManager _sceneManager;
+
+    Vector3 pos;
+
+    [SerializeField] TextMeshProUGUI textText;
+    private List<OVRSceneAnchor> anchors = new List<OVRSceneAnchor>();
+
+    private void Awake()
+    {
+        //ルーム設定の読み込みが成功した時のコールバック登録
+        _sceneManager.SceneModelLoadedSuccessfully += onSceneModelLoaded;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        floortranform();
+    }
+
+    private void onSceneModelLoaded()
+    {
+        //OVRSceneRoomの参照取得
+        OVRSceneRoom sceneRoom = FindAnyObjectByType<OVRSceneRoom>();
+        //天井
+        OVRScenePlane ceiling = sceneRoom.Ceiling;
+        //床
+        OVRScenePlane floor = sceneRoom.Floor;
+        //壁
+        OVRScenePlane[] walls = sceneRoom.Walls;
+
+        pos.y = floor.transform.position.y;
+    }
+
+    void floortranform()
+    {
+        gameObject.transform.position = new Vector3(this.transform.position.x, pos.y,this.transform.position.z);
+        textText.text = gameObject.transform.position.ToString();
+    }
+}
--- Assets/Project/Script/Jibiki/ParticleCollision.cs
+++ Assets/Project/Script/Jibiki/ParticleCollision.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 
 public class ParticleCollision : MonoBehaviour
 {
+    AudioManager audioM;
+
+    bool audioflag;
+
+    void Start()
+    {
+        audioM = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        audioflag = true;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Brush"))
         {
-            //Debug.Log("エフェクトに当たった");
-            Destroy(transform.parent.gameObject);
+            if(audioflag)
+            {
+                audioflag = false;
+                audioM.PlayPoint(audioM.data.hole, this.gameObject);
+            }
+            Debug.Log("音鳴った");
+            Destroy(transform.parent.gameObject, 0.4f);
         }
     }
 }
