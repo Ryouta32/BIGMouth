d5bcfabc896c720d6f2fff55459291d00fc9b4f0
Ryouta32 <ryouta32ryouta@gmail.com>
2025-01-05 16:07:05 +0900

    u-mu

--- Assets/Project/Script/Furukawa/tutorialScript.cs
+++ Assets/Project/Script/Furukawa/tutorialScript.cs
@@ -6,27 +6,46 @@ public class tutorialScript : MonoBehaviour
 {
     [SerializeField] float time;
     [SerializeField] GameObject fastBeta;
+    [SerializeField] GameObject Timeline;
     private GameObject obj;
+    AudioSource source;
+    private bool isAudio;
     void Start()
     {
-        obj = Instantiate(fastBeta,transform.position,Quaternion.identity);
-        obj.GetComponent<TutorialEnemy>().SetTutorial(this);
-        AudioManager.manager.PlayPoint(AudioManager.manager.data.announce, this.gameObject);
+
+        Timeline.SetActive(false);
+        //AudioManager.manager.PlayPoint(AudioManager.manager.data.announce, this.gameObject);
+        source = GetComponent<AudioSource>();
+        source.clip = AudioManager.manager.data.announce;
+        source.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!source.isPlaying && isAudio)
+        {
+            Play();
+        }
+    }
+    public void Play()
+    {
+        obj = Instantiate(fastBeta, transform.position, Quaternion.identity);
+        obj.GetComponent<TutorialEnemy>().SetTutorial(this);
     }
     public void Retry()
     {
         obj = Instantiate(fastBeta, transform.position, Quaternion.identity);
         obj.GetComponent<TutorialEnemy>().SetTutorial(this);
+        source.clip = AudioManager.manager.data.announce;
 
     }
     public void CLEAR()
     {
         //スタート処理。ドラゴンどうやったら時間弄れるんや
+        source.clip = AudioManager.manager.data.announce;
+        source.Play();
+        Timeline.SetActive(true);
         Debug.Log("げーむかいしー");
     }
 }
