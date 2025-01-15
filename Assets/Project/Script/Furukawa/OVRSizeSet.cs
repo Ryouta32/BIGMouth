ebef75f90da885e28c31a5a9b01aa83e0f2f9b6d
Ryouta32 <90999621+Ryouta32@users.noreply.github.com>
2025-01-15 18:17:16 +0900

    チュートリアル入れました

---
+++ Assets/Project/Script/Furukawa/OVRSizeSet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRSizeSet : MonoBehaviour
+{
+    [SerializeField] Vector3 size;
+    [SerializeField]GameObject obj;
+    GameObject mark;
+    void Start()
+    {
+        transform.parent = null;
+        transform.localScale = size ;
+        mark = Instantiate(obj, transform.position, Quaternion.identity);
+        //game.transform.parent = this.gameObject.transform;
+        mark.transform.localScale = size ;
+    }
+
+}
--- Assets/Project/Script/Furukawa/tutorialScript.cs
+++ Assets/Project/Script/Furukawa/tutorialScript.cs
@@ -5,11 +5,15 @@ public class tutorialScript : MonoBehaviour
 {
     [SerializeField] GameObject fastBeta;
     [SerializeField] SceneName.sceneName sceneName;
+    [SerializeField] Animator UIanima;
+    [SerializeField] TutorialUIScript uIScript;
     //[SerializeField] GameObject Timeline;
     GameObject obj;
     AudioSource source;
     bool isAudio = true;
     bool isClear=false;
+    bool isRetry = false;
+    bool uianima = false;
     void Start()
     {
         //Timeline.SetActive(false);
@@ -27,8 +31,8 @@ public class tutorialScript : MonoBehaviour
                 source.Stop();
                 Play();
                 //CLEAR();
-                if (Input.GetKeyDown(KeyCode.LeftShift))
-                    CLEAR();
+                //if (Input.GetKeyDown(KeyCode.LeftShift))
+                    //CLEAR();
             }
         }else
         if(!source.isPlaying && isAudio)
@@ -40,6 +44,15 @@ public class tutorialScript : MonoBehaviour
                 Destroy(obj);
             Destroy(gameObject);
         }
+
+        if (isRetry && !source.isPlaying&&uianima)
+        {
+            isRetry = false;
+            obj = Instantiate(fastBeta, transform.position, Quaternion.identity);
+            obj.GetComponent<TutorialEnemy>().SetTutorial(this);
+            source.clip = AudioManager.manager.data.announce;
+        }
+
     }
     public void Play()
     {
@@ -48,9 +61,13 @@ public class tutorialScript : MonoBehaviour
     }
     public void Retry()
     {
-        obj = Instantiate(fastBeta, transform.position, Quaternion.identity);
-        obj.GetComponent<TutorialEnemy>().SetTutorial(this);
-        source.clip = AudioManager.manager.data.announce;
+        isRetry = true;
+        UIanima.gameObject.SetActive(true);
+        uIScript.Retry();
+    }
+    public void SetState(tutorialUIState state)
+    {
+        uIScript.SetState(state);
     }
     public void CLEAR()
     {
@@ -60,6 +77,6 @@ public class tutorialScript : MonoBehaviour
         isAudio = true;
         //Timeline.SetActive(true);
         isClear = true;
-
     }
+    public void setanima(bool a) => uianima = a;
 }
