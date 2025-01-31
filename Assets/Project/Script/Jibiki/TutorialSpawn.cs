847d8e717448b7e4a4dd947d198ee85954739186
Fumirin16 <90897959+Fumirin16@users.noreply.github.com>
2025-01-31 13:08:22 +0900

    Update TutorialSpawn.cs

--- Assets/Project/Script/Jibiki/TutorialSpawn.cs
+++ Assets/Project/Script/Jibiki/TutorialSpawn.cs
@@ -36,7 +36,7 @@ public class TutorialSpawn : MonoBehaviour
         {
             if (classification.Contains(OVRSceneManager.Classification.WallArt))
             {
-                Vector3 pos = new Vector3(classification.transform.position.x, classification.transform.position.y - 0.4f, classification.transform.position.z);
+                Vector3 pos = new Vector3(classification.transform.position.x, floor.transform.position.y + 1.0f, classification.transform.position.z);
                 GameObject obj = Instantiate(Main, pos, Quaternion.identity);
                 obj.transform.localEulerAngles = new Vector3(0, 90, 0);
                 Tutorial.setStageAnima(obj.GetComponent<Animator>());
