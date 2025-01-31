6d6ddd3b18fbe755e31ad72b174ec24c1256c16d
Ryouta32 <90999621+Ryouta32@users.noreply.github.com>
2025-01-31 16:36:42 +0900

    スティックとチュートリアルといろいろ

--- Assets/Project/Script/Furukawa/BouUIScript.cs
+++ Assets/Project/Script/Furukawa/BouUIScript.cs
@@ -9,12 +9,16 @@ public class BouUIScript : MonoBehaviour
     [SerializeField] Image[] suikomis;
     [SerializeField] Image gage;
     [SerializeField] Animator Pikan;
+    [SerializeField] Image trigger;
+    [SerializeField] Sprite[] triggerSprites;
+    [SerializeField] float triggerTime;
     bool on;
     // Start is called before the first frame update
     void Start()
     {
         saki.GetComponent<BouSakiScript>();
         on = true;
+        StartCoroutine("Trigger");
     }
 
     // Update is called once per frame
@@ -33,6 +37,18 @@ public class BouUIScript : MonoBehaviour
                 suikomis[(i + 1) % suikomis.Length].color = Color.white;
             Pikan.SetTrigger("Pikan");
             on = true;
+        }
+    }
+    IEnumerator Trigger()
+    {
+        int count = 0 ;
+        while (true)
+        {
+            yield return new WaitForSeconds(triggerTime);
+            trigger.sprite = triggerSprites[count];
+            count++;
+            if(count ==triggerSprites.Length)
+                count = 0 ;
         }
     }
 }
